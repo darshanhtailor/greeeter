c482bbbb4c30f0145a66a7abbea9cbbb62a8a40c
Darshan Tailor <darshanhtailor@gmail.com>
2022-01-25 19:54:47 +0530

    just commit

--- public/js/getDevice.js
+++ public/js/getDevice.js
@@ -76,7 +76,7 @@ let video = 0, audio = 0
 videoButton.addEventListener('click', ()=>{
     if(!video){
         videoButton.innerHTML = '<i class="fa-solid fa-video"></i>'
-        videoButton.style.backgroundColor = 'rgb(50, 50, 50)'
+        videoButton.style.backgroundColor = 'black'
         video ^= 1
     } else{
         videoButton.innerHTML = '<i class="fa-solid fa-video-slash"></i>'
@@ -88,7 +88,7 @@ videoButton.addEventListener('click', ()=>{
 audioButton.addEventListener('click', ()=>{
     if(!audio){
         audioButton.innerHTML = '<i class="fa-solid fa-microphone"></i>'
-        audioButton.style.backgroundColor = 'rgb(50, 50, 50)'
+        audioButton.style.backgroundColor = 'black'
         audio ^= 1
     } else{
         audioButton.innerHTML = '<i class="fa-solid fa-microphone-slash"></i>'
